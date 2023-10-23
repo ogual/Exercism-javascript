2c0d03d42e8a4d20125b20ca6ac2fdea0a929701
ogual <ogual82@gmail.com>
2023-10-23 08:23:23 +0200

    Add Resistor-Color-Duo Exercise

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (a) => {
+  
+  const values = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  };
+
+  return parseInt(`${values[a[0]]}${values[a[1]]}`);
+  
+};
